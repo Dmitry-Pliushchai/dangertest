b8023aed434bdb65f41baab97b87b9714f72745f
Dzmitry Pliushchai <pliushchai@rushstreetinteractive.com>
2022-01-31 19:57:06 +0300

    merge

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,7 +1,10 @@
-import Danger 
+import Danger
+import DangerXCodeSummary // package: https://github.com/f-meloni/danger-swift-xcodesummary.git
+
 let danger = Danger()
 
 let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
 message("These files have changed: \(editedFiles.joined(separator: ", "))")
 
-
+let summary = XCodeSummary(filePath: "result.json")
+summary.report()
